87444c7f2c7c18f53aa7ea05483b0a7cf3ee0e1c
backgron <1319536124@qq.com>
2022-11-28 12:12:45 +0800

    add trimleft

---
+++ challenges/00106-medium-trimleft.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+  Expect<Equal<TrimLeft<"str">, "str">>,
+  Expect<Equal<TrimLeft<" str">, "str">>,
+  Expect<Equal<TrimLeft<"     str">, "str">>,
+  Expect<Equal<TrimLeft<"     str     ">, "str     ">>,
+  Expect<Equal<TrimLeft<"   \n\t foo bar ">, "foo bar ">>,
+  Expect<Equal<TrimLeft<"">, "">>,
+  Expect<Equal<TrimLeft<" \n\t">, "">>
+]
+
+// ============= Your Code Here =============
+type TrimLeft<S extends string> = S extends `${infer R}${infer T}`
+  ? R extends " " | "\n" | "\t"
+    ? TrimLeft<T>
+    : S
+  : S
+
+type A = TrimLeft<" str">
