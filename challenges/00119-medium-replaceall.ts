b0618311544117978d3f005a333c352e204b2452
backgron <1319536124@qq.com>
2022-11-30 12:19:54 +0800

    add replaceall

---
+++ challenges/00119-medium-replaceall.ts
@@ -0,0 +1,28 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils"
+
+type cases = [
+  Expect<Equal<ReplaceAll<"foobar", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobar", "bag", "foo">, "foobar">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "bar", "foo">, "foofoofoo">>,
+  Expect<Equal<ReplaceAll<"t y p e s", " ", "">, "types">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "", "foo">, "foobarbar">>,
+  Expect<Equal<ReplaceAll<"barfoo", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobarfoobar", "ob", "b">, "fobarfobar">>,
+  Expect<Equal<ReplaceAll<"foboorfoboar", "bo", "b">, "foborfobar">>,
+  Expect<Equal<ReplaceAll<"", "", "">, "">>
+]
+
+// ============= Your Code Here =============
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer R}${From}${infer T}`
+  ? `${ReplaceAll<R, From, To>}${To}${ReplaceAll<T, From, To>}`
+  : S
+
+type A = ReplaceAll<"foobar", "bar", "foo">
+//   ^?
